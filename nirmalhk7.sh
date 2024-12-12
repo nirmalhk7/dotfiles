516fa5d542daa87982200e77215c1ead7a0d71d4
Nirmal Khedkar <nirmalhk7@gmail.com>
2024-12-12 10:40:39 -0700

    Theme updates
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

--- nirmalhk7.sh
+++ nirmalhk7.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 alias code="/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code"
-alias p="python3"
+alias p="python"
 function hulksmash(){
     kill -9 $(lsof -t -i:$1)
 }
@@ -13,3 +13,24 @@ alias gcasm='git commit -asm'
 alias gst='git status'
 
 export PATH="/Users/nirmalhk7/Downloads/google-cloud-sdk/bin:$PATH"
+export PATH="/usr/local/go/bin:$PATH"
+
+function port_forward() {
+    local service_name=$1
+    local local_port=$2
+    local remote_port=$2
+    local namespace=${3:-default}
+
+    kubectl port-forward svc/$service_name -n $namespace $local_port:$remote_port &
+    local pid=$!
+
+    sleep 5
+
+    if ! ps -p $pid > /dev/null; then
+        echo "Port forwarding for service $service_name failed, restarting..."
+        kill -9 $pid
+        kubectl port-forward svc/$service_name -n $namespace $local_port:$remote_port &
+    else
+        echo "Port forwarding for service $service_name setup successfully."
+    fi
+}
