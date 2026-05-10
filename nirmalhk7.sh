39910967c1ff2f97af423c4e047171a8eed08ba4
Nirmal Khedkar <nirmalhk7@gmail.com>
2026-05-10 17:27:31 -0600

    chore(repo): Clean up repository and update gitignore
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

--- nirmalhk7.sh
+++
@@ -1,6 +0,0 @@
-# Compatibility bridge to new modular config
-DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-source "$DOTFILES_DIR/zsh/env.zsh"
-source "$DOTFILES_DIR/zsh/aliases.zsh"
-source "$DOTFILES_DIR/zsh/functions.zsh"
